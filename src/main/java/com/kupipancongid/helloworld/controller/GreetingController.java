596681ea2f6c2854b594381a0dccee231df075e6
kupipancongid <idkupipancong@gmail.com>
2023-09-25 22:41:30 +0700

    return a hello world message

---
+++ src/main/java/com/kupipancongid/helloworld/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.kupipancongid.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping(path = "/")
+    public String index(){
+        return "Hello World!";
+    }
+}
