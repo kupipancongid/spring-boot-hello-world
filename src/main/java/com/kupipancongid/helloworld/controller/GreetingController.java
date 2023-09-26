60be7cee0f94e08f0d8509c8921273da87a90287
kupipancongid <idkupipancong@gmail.com>
2023-09-26 09:03:55 +0700

    adding hello url with optional param

--- src/main/java/com/kupipancongid/helloworld/controller/GreetingController.java
+++ src/main/java/com/kupipancongid/helloworld/controller/GreetingController.java
@@ -2,13 +2,23 @@ package com.kupipancongid.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
     @GetMapping(path = "/")
     public String index(){
-        return "Hello World!";
+        return "Hello World! Welcome to our website.";
+    }
+    @GetMapping(path = "/hello")
+    public String sayHello(
+            @RequestParam("name") String name
+    ){
+        if (name.equals("")){
+            return "Hello, guest.";
+        }
+        return "Hello, "+name;
     }
 }
